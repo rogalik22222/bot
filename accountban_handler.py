1566dbd44e11904a7e540b03168b2e33d1ba7117
rogalik22222 <114074998+rogalik22222@users.noreply.github.com>
2024-06-18 19:58:40 +0300

    chaged account. add loggs all

--- accountban_handler.py
+++ accountban_handler.py
@@ -4,79 +4,86 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from telegram import Update, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext, CallbackQueryHandler
-import asyncio
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext, \
+    CallbackQueryHandler
 import os
 from config import COOKIES
-logging.basicConfig(level=logging.INFO)
-from  database import *
-NICKNAMES, SERVER, PROCESSING = range(3)
+from database import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import asyncio
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%d.%m.%Y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+NICKNAMES, SERVER = range(2)
+
 
 async def account_start(update: Update, context: CallbackContext) -> int:
     user_id = update.message.from_user.id
     role = get_user_role(user_id)
     if role == 'admin' or role == 'developer':
+        logger.info(f"Пользователь {user_id} начал проверку кандидатов")
         await update.message.reply_text('Введите никнеймы (каждый новый ник на новой строке):')
         return NICKNAMES
     else:
+        logger.warning(
+            f"Пользователь {user_id} с ролью {role} попытался запустить проверку кандидатов, но ему было отказано в доступе")
         await update.message.reply_text('Отказано в доступе')
         return ConversationHandler.END
 
 
 async def get_server_choice(update: Update, context: CallbackContext) -> int:
+    user_id = update.message.from_user.id
     nicknames = update.message.text.strip().split('\n')
     context.user_data['nicknames'] = [nick.strip() for nick in nicknames if nick.strip()]
 
+    logger.info(f"Пользователь {user_id} ввел ники: {context.user_data['nicknames']}")
+
     keyboard = [
-        [InlineKeyboardButton(f"{i}", callback_data=str(i)) for i in range(1, 8)]  # assuming 10 servers
+        [InlineKeyboardButton(f"{i}", callback_data=str(i)) for i in range(1, 8)]  # предполагаем 7 серверов
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text('Выберите номер сервера:', reply_markup=reply_markup)
     return SERVER
 
+
 async def server_chosen(update: Update, context: CallbackContext) -> int:
+    user_id = update.callback_query.from_user.id
     query = update.callback_query
     await query.answer()
     server = query.data
     context.user_data['server'] = server
+
+    logger.info(f"Пользователь {user_id} выбрал сервер №: {server}")
 
     await query.edit_message_text(text=f"Вы выбрали сервер {server}. Начинаю обработку...")
 
     nicknames = context.user_data.get('nicknames', [])
     server = context.user_data.get('server')
 
-    results = await process_nicknames(query, context, nicknames, server)
+    results = await process_nicknames(update, context, nicknames, server)
 
     file_path = save_results_to_file(results)
 
     if os.path.getsize(file_path) > 0:
         await context.bot.send_document(chat_id=update.effective_chat.id, document=open(file_path, 'rb'))
     else:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Не удалось найти информацию для предоставленных никнеймов.")
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="Не удалось найти информацию для предоставленных никнеймов.")
 
-    os.remove(file_path)  # Clean up the file after sending
+    os.remove(file_path)  # Очистка файла после отправки
 
     return ConversationHandler.END
 
-async def process_nicknames(query, context: CallbackContext, nicknames, server):
+
+async def process_nicknames(update: Update, context: CallbackContext, nicknames, server):
+    user_id = update.effective_user.id
     results = []
-
-    for nick in nicknames:
-        player_id, reg_ip, last_ip = await get_player_info(query, context, nick, server)
-
-        if player_id:
-            related_ids = await find_related_accounts(query, context, server, reg_ip, last_ip)
-            for related_id in related_ids:
-                logs = await get_account_logs(query, context, server, related_id)
-                results.append({'nick': nick, 'logs': logs})
-
-    return results
-
-async def get_player_info(query, context: CallbackContext, nick, server):
-    logging.info("Запуск браузера для получения информации об аккаунте")
-    await query.message.reply_text(f'Начал поиск информации для аккаунта {nick}')
-
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')  # только если необходимо
@@ -84,16 +91,39 @@ async def get_player_info(query, context: CallbackContext, nick, server):
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
 
-    driver = webdriver.Chrome(service=service, options=options)
+    with webdriver.Chrome(service=service, options=options) as driver:
+        for nick in nicknames:
+            logger.info(f"Пользователь {user_id} ищет аккаунт: {nick} на сервере {server}")
+            player_id, reg_ip, last_ip = await get_player_info(update, context, driver, nick, server)
+
+            if player_id:
+                related_ids = await find_related_accounts(update, context, driver, reg_ip, last_ip)
+                for related_id in related_ids:
+                    logs = await get_account_logs(update, context, driver, related_id)
+                    results.append({'nick': nick, 'logs': logs})
+
+    return results
+
+
+async def get_player_info(update: Update, context: CallbackContext, driver, nick, server):
+    user_id = update.effective_user.id
+    logger.info(f"Пользователь {user_id} начал получение информации об аккаунте {nick} на сервере {server}")
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Начал поиск информации для аккаунта {nick}')
+
     url = f"https://rodina.logsparser.info/accounts?server_number={server}&name={nick}+"
 
     try:
         driver.get(url)
+        # Убедитесь, что вы находитесь на правильном домене перед добавлением куки
+        current_domain = driver.current_url.split('/')[2]
         for cookie in COOKIES:
+            if 'domain' in cookie and cookie['domain'] not in current_domain:
+                continue
             driver.add_cookie(cookie)
         driver.get(url)  # Перезагрузка страницы после добавления куки
-        time.sleep(15)
-        logging.info(url)
+        WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((By.TAG_NAME, "td")))
+        logger.info(url)
 
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
@@ -101,40 +131,39 @@ async def get_player_info(query, context: CallbackContext, nick, server):
 
         if len(rows) >= 9:
             player_id = rows[0].get_text().strip()
-            last_ip = rows[-3].get_text().strip()  # Third to last <tr>
-            reg_ip = rows[-2].get_text().strip()  # Second to last <tr>
-            logging.info(f"Получена информация для {nick}: player_id={player_id}, reg_ip={reg_ip}, last_ip={last_ip}")
+            last_ip = rows[-3].get_text().strip()  # Третий с конца <tr>
+            reg_ip = rows[-2].get_text().strip()  # Второй с конца <tr>
+            logger.info(f"Получена информация для {nick}: player_id={player_id}, reg_ip={reg_ip}, last_ip={last_ip}")
             return player_id, reg_ip, last_ip
         else:
-            logging.warning(f"Ожидаемая структура таблицы не найдена для {nick}")
+            logger.warning(f"Ожидаемая структура таблицы не найдена для {nick}")
             return None, None, None
-    finally:
-        driver.quit()
+    except Exception as e:
+        logger.error(f"Ошибка при получении информации для {nick}: {e}")
+        return None, None, None
 
-async def find_related_accounts(query, context: CallbackContext, server, reg_ip, last_ip):
-    logging.info(f"Поиск связанных аккаунтов по IP: {reg_ip} и {last_ip}")
 
+async def find_related_accounts(update: Update, context: CallbackContext, driver, reg_ip, last_ip):
+    user_id = update.effective_user.id
+    logger.info(f"Пользователь {user_id} ищет связанные аккаунты по IP: {reg_ip} и {last_ip}")
+
+    gaserver = get_server(user_id)
     related_ids = set()
     for ip in [reg_ip, last_ip]:
         if not ip:
             continue
 
-        service = Service(ChromeDriverManager().install())
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')  # только если необходимо
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-gpu')
-
-        driver = webdriver.Chrome(service=service, options=options)
-        url = f"https://rodina.logsparser.info/accounts?server_number=5&ip={ip}"
+        url = f"https://rodina.logsparser.info/accounts?server_number={gaserver}&ip={ip}"
 
         try:
             driver.get(url)
+            current_domain = driver.current_url.split('/')[2]
             for cookie in COOKIES:
+                if 'domain' in cookie and cookie['domain'] not in current_domain:
+                    continue
                 driver.add_cookie(cookie)
             driver.get(url)  # Перезагрузка страницы после добавления куки
-            time.sleep(15)
+            WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((By.TAG_NAME, "tr")))
 
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
@@ -144,61 +173,66 @@ async def find_related_accounts(query, context: CallbackContext, server, reg_ip,
                 cells = row.find_all('td')
                 if cells:
                     related_ids.add(cells[0].get_text().strip())
-        finally:
-            driver.quit()
+        except Exception as e:
+            logger.error(f"Ошибка при поиске связанных аккаунтов по IP {ip}: {e}")
 
-    logging.info(f"Найдены связанные аккаунты: {related_ids}")
+    logger.info(f"Найдено связанных аккаунтов: {related_ids}")
     return related_ids
 
-async def get_account_logs(query, context: CallbackContext, server, player_id):
-    logging.info(f"Получение логов для аккаунта {player_id}")
 
-    service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # только если необходимо
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
+async def get_account_logs(update: Update, context: CallbackContext, driver, player_id):
+    user_id = update.effective_user.id
+    logger.info(f"Пользователь {user_id} начал получение логов для аккаунта {player_id}")
 
-    driver = webdriver.Chrome(service=service, options=options)
-    url = f"https://rodina.logsparser.info/?server_number=5&type%5B%5D=ban&type%5B%5D=jail&type%5B%5D=mute&type%5B%5D=banip&type%5B%5D=unban&type%5B%5D=unjail&type%5B%5D=unmute&type%5B%5D=unbanip&sort=desc&limit=1000&player={player_id}"
+    gaserver = get_server(user_id)
+
+    url = f"https://rodina.logsparser.info/?server_number={gaserver}&type%5B%5D=ban&type%5B%5D=jail&type%5B%5D=mute&type%5B%5D=banip&type%5B%5D=unban&type%5B%5D=unjail&type%5B%5D=unmute&type%5B%5D=unbanip&sort=desc&limit=1000&player={player_id}"
 
     try:
         driver.get(url)
+        current_domain = driver.current_url.split('/')[2]
         for cookie in COOKIES:
+            if 'domain' in cookie and cookie['domain'] not in current_domain:
+                continue
             driver.add_cookie(cookie)
         driver.get(url)  # Перезагрузка страницы после добавления куки
-        time.sleep(15)
+        WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((By.TAG_NAME, "tr")))
 
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         rows = soup.find_all('tr')
-
         logs = []
         for row in rows:
-            cells = row.find_all('td')[:-2]
+            cells = row.find_all('td')[:-2]  # Ищем все клетки в строке, кроме последних двух
             log_line = ' '.join(cell.get_text().strip() for cell in cells)
             logs.append(log_line)
 
-        logging.info(f"Логи для аккаунта {player_id}: {logs}")
+        logger.info(f"Получены логи для аккаунта {player_id}: {logs}")
         return logs
-    finally:
-        driver.quit()
+    except Exception as e:
+        logger.error(f"Ошибка при получении логов для аккаунта {player_id}: {e}")
+        return []
+
+
 def save_results_to_file(results):
     file_path = 'results.txt'
     with open(file_path, 'w', encoding='utf-8') as file:
         for result in results:
-            file.write(f"Nick_Name: {result['nick']}\n")
+            file.write(f"Найдено наказаний по никнейму: {result['nick']}\n")
             for log in result['logs']:
                 file.write(f"{log}\n")
             file.write("\n")
     return file_path
 
+
 async def account_cancel(update: Update, context: CallbackContext) -> int:
     from main import start
+    user_id = update.message.from_user.id
+    logger.info(f"Пользователь {user_id} отменил команду.")
     await update.message.reply_text('Команда отменена.')
     await start(update, context)
     return ConversationHandler.END
+
 
 def accountcc_handler():
     return ConversationHandler(
@@ -209,3 +243,6 @@ def accountcc_handler():
         },
         fallbacks=[CommandHandler('cancel', account_cancel)],
     )
+
+
+
