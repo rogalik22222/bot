43f7faf59c38c17cc75e62798f298a7257e12745
rogalik22222 <114074998+rogalik22222@users.noreply.github.com>
2024-07-01 12:32:28 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -38,10 +38,11 @@ def update_user_role(user_id, new_role):
         cursor.execute("UPDATE users SET role = ? WHERE user_id = ?", (new_role, user_id))
         conn.commit()
 
-def get_all_users():
+def get_all_users(order_by="server", order="ASC"):
     with sqlite3.connect("database.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT user_id, telegram_id, nickname, role, server FROM users")
+        query = f"SELECT user_id, telegram_id, nickname, role, server FROM users ORDER BY {order_by} {order}"
+        cursor.execute(query)
         return cursor.fetchall()
 
 def delete_user(user_id):
@@ -57,11 +58,12 @@ def get_server(user_id):
         result = cursor.fetchone()
         return result[0] if result else None
 
-def get_all_users_admin():
-    with sqlite3.connect("database.db") as conn:
-         cursor = conn.cursor()
-         cursor.execute("SELECT user_id, nickname, role, server FROM users")
-         return cursor.fetchall()
+def get_all_users_admin(order_by="server", order="ASC"):
+     with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        query = f"SELECT user_id, nickname, role, server FROM users ORDER BY {order_by} {order}"
+        cursor.execute(query)
+        return cursor.fetchall()
 
 
 
